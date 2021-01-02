6f1411d8a23d025586fc401523ba2004fedddd1a
Andre Paul <andrepaul8128@gmail.com>
2021-01-02 14:39:28 -0800

    some more redux

--- App.js
+++ App.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as firebase from 'firebase'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
-import * as firebase from 'firebase'
+import MainScreen from './components/Main'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -69,9 +76,9 @@ export class App extends Component {
     }
 
     return(
-      <View style={{ flex: 1, justifyContent: 'center'}}>
-        <Text>User is logged in</Text>
-      </View>
+      <Provider store={store}>
+        <MainScreen/>
+      </Provider>
     )
   }
 }
